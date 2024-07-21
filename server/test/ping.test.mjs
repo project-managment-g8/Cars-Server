1eef16c506b88b7b1b6ced3efc2cf173af771655
liavjulio <liavjulio7@gmail.com>
2024-07-21 16:00:04 +0300

    improve test

--- server/test/ping.test.mjs
+++ server/test/ping.test.mjs
@@ -1,12 +1,15 @@
 import request from 'supertest';
-import { app } from '../server.js'; // Ensure correct path
+import { app } from '../server.js';
 
-describe('GET /ping', () => {
-  it('should return pong <G8>', (done) => {
+describe('GET /ping', function() {
+  it('should return pong <G8>', function(done) {
     request(app)
       .get('/ping')
       .expect(200)
       .expect('pong <G8>')
-      .end(done);
+      .end((err, res) => {
+        if (err) return done(err);
+        done();
+      });
   });
 });
