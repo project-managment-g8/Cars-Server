23851fd96f649b23c9bc3a2d309096b7c2358a55
liavjulio <liavjulio7@gmail.com>
2024-07-30 12:54:19 +0300

    lint fix

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,16 @@
-import globals from 'globals';
-import js from '@eslint/js';
-import node from 'eslint-plugin-node';
+import globals from "globals";
+import js from "@eslint/js";
+import node from "eslint-plugin-node";
 
 export default [
   {
-    files: ['server/**/*.js'],
+    files: ["server/**/*.js"],
     languageOptions: { globals: globals.node },
     rules: {
-     'indent': ['error', 2],
-      'linebreak-style': ['error', 'unix'],
-      'quotes': ['error', 'single'],
-      'semi': ['error', 'always'],
-      'no-console': 0,
+      indent: ["warn", 2],
+      "linebreak-style": ["error", "unix"],
+      semi: ["error", "always"],
+      "no-console": 0,
     },
-  }
+  },
 ];
