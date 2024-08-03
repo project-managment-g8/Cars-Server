6b978956f4aa00d602600343e149c5c9a6d3898f
liavjulio <liavjulio7@gmail.com>
2024-08-03 21:10:46 +0300

    token fix

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -1,4 +1,8 @@
 import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+// Load environment variables from .env file
+dotenv.config();
 
 const generateToken = (res, userId) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
