8ab1c77757586ffa71561becb014b385ecfaeef0
liavjulio <liavjulio7@gmail.com>
2024-08-03 21:18:46 +0300

    fix

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -4,16 +4,18 @@ import dotenv from "dotenv";
 // Load environment variables from .env file
 dotenv.config();
 
+// Set default value for NODE_ENV if not set
+const nodeEnv = process.env.NODE_ENV || "development";
+
 const generateToken = (res, userId) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: "30d",
   });
 
-  // Send the token as an HTTP-only cookie
   res.cookie("token", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production", // Use secure cookies in production
-    sameSite: "strict", // Prevent CSRF attacks
+    secure: nodeEnv === "production", // Use secure cookies in production
+    sameSite: nodeEnv === "production" ? "none" : "strict", // Adjust SameSite policy
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
   });
 };
